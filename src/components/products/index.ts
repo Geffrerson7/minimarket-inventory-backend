771c593a4a32ff2ed7c1bbea676e0562b349f485
reyalexander <alexandercayromamani@gmail.com>
2023-02-02 10:42:04 -0500

    CRUD of Products component

--- src/components/products/index.ts
+++ src/components/products/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { store } from "./controller";
+import { store, findAll,get_product_by_id, update_product, delete_product_by_id } from "./controller";
 
 const productRouter: Router = Router();
 
 productRouter.post("/", store);
+productRouter.get("/", findAll);
+productRouter.get("/:id", get_product_by_id);
+productRouter.put("/:id", update_product);
+productRouter.delete("/:id", delete_product_by_id);
 
 export default productRouter;
